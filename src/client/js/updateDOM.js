141681dfbf35a3835cf5ad106309724807fe820c
Omar Sakr <mero.saker@gmail.com>
2021-04-25 22:04:39 +0200

    Add function to update the DOM

--- src/client/js/updateDOM.js
+++ src/client/js/updateDOM.js
@@ -5,5 +5,56 @@ const confidence = document.querySelector("#confidence")
 const irony = document.querySelector("#irony")
 
 export function updateDOM(data){
-        console.log(data.agreement, data.confidence , data.irony)
+        // Update dom element with data from the server
+
+        scoreTag.innerHTML = convertScoreTag(data.scoreTag)
+        agreement.innerHTML = data.agreement
+        subjectivity.innerHTML = data.subjectivity
+        confidence.innerHTML = data.confidence + '%'
+        irony.innerHTML = data.irony
+
+}
+
+
+
+/**
+*       Score tag can be translated into:
+
+*      P+: strong positive
+*      P: positive
+*      NEU: neutral
+*      N: negative
+*      N+: strong negative
+*      NONE: without polarity
+ */
+function convertScoreTag(score){
+        let convertedScore
+
+        switch(score){
+                case 'P+':
+                        convertedScore = 'Strong Positive'
+                        break;
+
+                case 'P':
+                        convertedScore = 'Positive'
+                        break;
+
+                case 'NEU':
+                        convertedScore = 'Neutral'
+                        break;
+
+                case 'N':
+                        convertedScore = 'Negative'
+                        break;
+
+                case 'N+':
+                        convertedScore = 'Strong Negative'
+                        break;
+
+                default:
+                        convertedScore= 'No polarity'
+        }
+
+        return convertedScore
+
 }
