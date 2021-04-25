42d073fd8ad26d6c8748cf533dfc435dcf51418b
Omar Sakr <mero.saker@gmail.com>
2021-04-25 21:23:09 +0200

    Fix : server issue

--- src/server/index.js
+++ src/server/index.js
@@ -1,27 +1,42 @@
 var path = require('path')
 const express = require('express')
-// const bodyParser = require('body-parser')
+var bodyParser = require('body-parser')
+var cors = require('cors')
+const { text } = require('express')
+
+var json = {
+    'title': 'test json response',
+    'message': 'this is a message',
+    'time': 'now'
+}
 
 const app = express()
-
-app.use(express.urlencoded())
+app.use(cors())
+// to use json
 app.use(express.json())
+// to use url encoded values
+app.use(express.urlencoded({
+  extended: true
+}))
 
 app.use(express.static('dist'))
 
-console.log(__dirname)
-
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
 })
+
+app.post('/analyse', (req, res) => {
+    console.log('Request recived')
+    let text = req.body.text
+    console.log(text)
+
+    res.send(JSON.stringify({message: ' recived'}))
+
+})
+
+
 
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
     console.log('Example app listening on port 8081!')
-})
-
-app.get("/analyse", (req, res) => {
-    let txt = req.body.txt
-    console(txt)
 })
