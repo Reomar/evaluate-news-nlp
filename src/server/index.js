bc264b433f6d0c2dbfb98dc518f67c8d9b95255f
Omar Sakr <mero.saker@gmail.com>
2021-04-26 04:43:22 +0200

    style: removed white spaces

--- src/server/index.js
+++ src/server/index.js
@@ -1,59 +1,57 @@
-var path = require('path')
-const express = require('express')
-var bodyParser = require('body-parser')
-var cors = require('cors')
-require('dotenv').config();
-const fetch = require('node-fetch');
+var path = require("path");
+const express = require("express");
+var bodyParser = require("body-parser");
+var cors = require("cors");
+require("dotenv").config();
+const fetch = require("node-fetch");
 
-
-
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
 // to use json
-app.use(express.json())
+app.use(express.json());
 // to use url encoded values
-app.use(express.urlencoded({
-  extended: true
-}))
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
-app.use(express.static('dist'))
+app.use(express.static("dist"));
 
-app.get('/', function (req, res) {
-    res.sendFile('dist/index.html')
-})
+app.get("/", function (req, res) {
+  res.sendFile("dist/index.html");
+});
 
-app.post('/analyse', (req, res) => {
-    console.log('Request Received...')
-    let text = req.body.text
+app.post("/analyse", (req, res) => {
+  console.log("Request Received...");
+  let text = req.body.text;
 
-    // Get Text Analysis from Meaning Cloud Sentiment Analysis API version 2.1
-    // https://learn.meaningcloud.com/developer/sentiment-analysis/2.1/doc/what-is-sentiment-analysis
+  // Get Text Analysis from Meaning Cloud Sentiment Analysis API version 2.1
+  // https://learn.meaningcloud.com/developer/sentiment-analysis/2.1/doc/what-is-sentiment-analysis
 
+  const url = `https://api.meaningcloud.com/sentiment-2.1?key=${process.env.API_KEY}&of=json&txt=${text}&lang=auto`;
 
+  fetch(url)
+    .then((res) => res.json())
+    .then((body) => {
+      console.log("API Data Sent...");
+      console.log("--------------------");
 
-    const url = `https://api.meaningcloud.com/sentiment-2.1?key=${process.env.API_KEY}&of=json&txt=${text}&lang=auto`
-
-    fetch(url)
-    .then(res => res.json())
-    .then(body => {
-        console.log('API Data Sent...')
-        console.log('--------------------')
-
-        // Send the API Data to the font-end to be updated in the DOM
-        res.send(JSON.stringify({
-            scoreTag : body.score_tag,
-            agreement : body.agreement,
-            subjectivity: body.subjectivity,
-            confidence: body.confidence,
-            irony: body.irony
-        }))
+      // Send the API Data to the font-end to be updated in the DOM
+      res.send(
+        JSON.stringify({
+          scoreTag: body.score_tag,
+          agreement: body.agreement,
+          subjectivity: body.subjectivity,
+          confidence: body.confidence,
+          irony: body.irony,
+        })
+      );
     })
-    .catch(err => console.log(err))
-})
-
-
+    .catch((err) => console.log(err));
+});
 
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
-    console.log('Example app listening on port 8081!')
-})
+  console.log("Example app listening on port 8081!");
+});
