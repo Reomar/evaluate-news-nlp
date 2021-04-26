07a35f77e926532f1f177529b27ca9495136e048
Omar Sakr <mero.saker@gmail.com>
2021-04-26 04:42:19 +0200

    Feat: Use external API to analyse text

--- src/server/index.js
+++ src/server/index.js
@@ -2,13 +2,10 @@ var path = require('path')
 const express = require('express')
 var bodyParser = require('body-parser')
 var cors = require('cors')
-const { text } = require('express')
+require('dotenv').config();
+const fetch = require('node-fetch');
 
-var json = {
-    'title': 'test json response',
-    'message': 'this is a message',
-    'time': 'now'
-}
+
 
 const app = express()
 app.use(cors())
@@ -26,19 +23,32 @@ app.get('/', function (req, res) {
 })
 
 app.post('/analyse', (req, res) => {
-    console.log('Request Received')
+    console.log('Request Received...')
     let text = req.body.text
 
-    // Get text 
+    // Get Text Analysis from Meaning Cloud Sentiment Analysis API version 2.1
+    // https://learn.meaningcloud.com/developer/sentiment-analysis/2.1/doc/what-is-sentiment-analysis
 
-    res.send(JSON.stringify({
-        scoreTag : 'scoreTag data',
-        agreement : 'agreement data',
-        subjectivity: 'subjectivity data',
-        confidence: 'confidence data',
-        irony: 'irony data'
-    }))
 
+
+    const url = `https://api.meaningcloud.com/sentiment-2.1?key=${process.env.API_KEY}&of=json&txt=${text}&lang=auto`
+
+    fetch(url)
+    .then(res => res.json())
+    .then(body => {
+        console.log('API Data Sent...')
+        console.log('--------------------')
+
+        // Send the API Data to the font-end to be updated in the DOM
+        res.send(JSON.stringify({
+            scoreTag : body.score_tag,
+            agreement : body.agreement,
+            subjectivity: body.subjectivity,
+            confidence: body.confidence,
+            irony: body.irony
+        }))
+    })
+    .catch(err => console.log(err))
 })
 
 
