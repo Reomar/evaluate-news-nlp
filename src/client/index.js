601b556d7719ce3b628766688cd6d36a065204a3
Omar Sakr <mero.saker@gmail.com>
2021-04-27 19:59:47 +0200

    Add styles

--- src/client/index.js
+++ src/client/index.js
@@ -1,2 +1,7 @@
 import "../client/js/analyse.js"
 
+// Import styles
+import "../client/styles/header.sass"
+import "../client/styles/base.sass"
+import "../client/styles/input.sass"
+import "../client/styles/results.sass"
